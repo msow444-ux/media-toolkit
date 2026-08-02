eef30f3415c06dc5bd0a6fb8db931451e38e0ead
msow444-ux <msow444@gmail.com>
2026-08-02 18:54:31 +0200

    Create class-media-toolkit.php
    Add includes directory

---
+++ includes/class-media-toolkit.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Główna klasa wtyczki Media Toolkit.
+ *
+ * @package MediaToolkit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+class Media_Toolkit {
+
+	/**
+	 * Uruchamia wtyczkę.
+	 *
+	 * @return void
+	 */
+	public function run() {
+
+	}
+
+}
