d07c8f34c1e027135a9dfdced15d7a27e28e383a
msow444-ux <msow444@gmail.com>
2026-08-02 16:06:34 +0200

    Create media-toolkit.php
    Add main plugin file

---
+++ media-toolkit.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Media Toolkit
+ * Plugin URI:        https://github.com/msow444-ux/media-toolkit
+ * Description:       Zestaw narzędzi do zarządzania obrazami i plikami multimedialnymi w WordPress.
+ * Version:           0.1.0
+ * Requires at least: 6.5
+ * Requires PHP:      8.0
+ * Author:            Marek Sowiński
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       media-toolkit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Wersja wtyczki.
+ */
+define( 'MEDIA_TOOLKIT_VERSION', '0.1.0' );
+
+/**
+ * Ścieżka do katalogu wtyczki.
+ */
+define( 'MEDIA_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
+
+/**
+ * Adres URL katalogu wtyczki.
+ */
+define( 'MEDIA_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
